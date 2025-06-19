a49132e30f904c82ac2b07a5c19badefdbf9057e
Mark Blakley <mark.blakley@gmail.com>
2025-06-19 17:30:51 -0400

    Clean up logging of requests

--- video_grouper/service/main.py
+++ video_grouper/service/main.py
@@ -21,6 +21,7 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 )
+logging.getLogger("httpx").setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
 
 class VideoGrouperService(win32serviceutil.ServiceFramework):
--- video_grouper/tray/main.py
+++ video_grouper/tray/main.py
@@ -24,6 +24,7 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 )
+logging.getLogger("httpx").setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
 
 class UpdateChecker(threading.Thread):
